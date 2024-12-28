ae27b5988a2a78ec15219860feec25b7ea86169d
Deepanshu Aggarwal <deepanshu.26.da@gmail.com>
2024-12-28 18:48:02 +0530

    first commit

---
+++ noteX-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      Click on the Vite and React logos to learn more
+    </div>
+  )
+}
+
+export default App
